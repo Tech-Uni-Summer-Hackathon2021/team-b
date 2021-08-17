eb3c118f396a92258cc811b1c57006de39a1deda
小濱 祐輔 <>
2021-08-18 00:43:39 +0900

    環境調整

--- Server/test.py
+++ Server/test.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.keys import Keys
 #ActionChainsを使う時は、下記のようにActionChainsのクラスをロードする
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import Select
-from bs4 import BeautifulSoup, BeautifulStoneSoup
+#from bs4 import BeautifulSoup, BeautifulStoneSoup
 from selenium.webdriver.chrome.options import Options
 
 # mac環境用
@@ -21,18 +21,18 @@ class Test:
         # self.options.add_argument('--headless')
         self.options.add_argument('--window-size=1920,1080')
 
-        #windows環境用
+        #Tsukasa環境用
         # self.driver = webdriver.Chrome('C:\work\chromedriver_win32\chromedriver')
 
-        #mac環境用
+        #Yusuke環境用
         self.driver = webdriver.Chrome(chrome_options=self.options)
 
     def act(self,url):
         self.driver.get(url)
 
         # 処理が終わったらwindowを閉じる
-        self.driver.close()
-        self.driver.quit()
+        #self.driver.close()
+        #self.driver.quit()
 
         # return json
 if __name__ == '__main__':
