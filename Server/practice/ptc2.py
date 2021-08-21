f8c379cc1b2f9c61f96e2a56a4bbaf3da420f769
小濱 祐輔 <>
2021-08-22 01:40:43 +0900

    練習2にヘッドレスのオプション追加

--- Server/practice/ptc2.py
+++ Server/practice/ptc2.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
 from time import sleep
+from selenium.webdriver.chrome import options
 
+from selenium.webdriver.chrome.options import Options
+options = Options()
+# options.add_argument('--headless')
 
-driver = webdriver.Chrome()
-
-#ブラウザ立ち上げは処理が重たくなるので本番環境ではheadlessモードを採用
-  # self.options.add_argument('--headless')
+driver = webdriver.Chrome(options = options) 
 
 url = ('https://syllabus.kwansei.ac.jp/uniasv2/UnSSOLoginControlFree')
 driver.get(url)
