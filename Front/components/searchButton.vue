a680ed4fb1925ad26e1523f9d386b8e03a4ae5ea
sannja <jnhtoisannja@gmail.com>
2021-08-21 23:43:21 +0900

    searchButton created

---
+++ Front/components/searchButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <v-text-field
+      rounded
+      solo
+      label="Search"
+      append-icon="mdi-magnify"
+      @click:append="clickAction"
+    ></v-text-field>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchButton',
+  methods: {
+    clickAction() {
+      alert('ha ha pl ya ml')
+    },
+  },
+}
+</script>
+
+<style scoped></style>
--- Front/nuxt.config.js
+++ Front/nuxt.config.js
@@ -66,7 +66,6 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
       themes: {
         dark: {
           primary: colors.blue.darken2,
