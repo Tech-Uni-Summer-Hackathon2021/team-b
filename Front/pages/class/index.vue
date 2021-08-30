2cf48682db115db3eeb3d8d3785afdbc089d189c
sannja <jnhtoisannja@gmail.com>
2021-08-30 22:59:00 +0900

    searchButton created

---
+++ Front/pages/class/index.vue
@@ -0,0 +1,95 @@
+<template>
+  <v-data-table :headers="headers" :items="desserts" class="table">
+    <template #[`item.oldq`]="{ item }">
+      <v-simple-checkbox v-model="item.oldq" disabled />
+    </template>
+    <template #[`item.review`]="{ item }">
+      <v-rating
+        v-model="item.review"
+        half-increments
+        background-color="orange lighten-3"
+        color="orange"
+        small
+        style="pointer-events: none"
+      />
+    </template>
+  </v-data-table>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data() {
+    return {
+      desserts: [
+        {
+          name: 'Frozen Yogurt',
+          review: 4.5,
+          day: 6.0,
+          period: 24,
+          oldq: true,
+        },
+        {
+          name: 'Ice cream sandwich',
+          review: 2,
+          day: 9.0,
+          period: 37,
+          oldq: false,
+        },
+        {
+          name: 'Eclair',
+          review: 2,
+          day: 16.0,
+          period: 23,
+          oldq: false,
+        },
+        {
+          name: 'Jelly bean',
+          review: 3,
+          day: 0.0,
+          period: 94,
+          oldq: true,
+        },
+        {
+          name: 'Lollipop',
+          review: 3,
+          day: 0.2,
+          period: 98,
+          oldq: true,
+        },
+        {
+          name: 'KitKat',
+          review: 5,
+          day: 26.0,
+          period: 65,
+          oldq: false,
+        },
+      ],
+      headers: [
+        {
+          text: '授業名',
+          align: 'start',
+          value: 'name',
+        },
+        { text: 'レビュー', value: 'review' },
+        { text: '過去問', value: 'oldq' },
+        { text: '曜日', value: 'day' },
+        { text: '時限', value: 'period' },
+      ],
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.table {
+  backdrop-filter: blur(2px);
+  background-color: rgba(255, 255, 255, 0.8);
+}
+</style>
+<style>
+.v-data-table-header {
+  backdrop-filter: blur(2px);
+  background-color: rgba(248, 185, 185, 0.4) !important;
+}
+</style>
---
+++ Front/pages/review/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="tab-container">
+    <v-tabs color="pink lighten-3" right>
+      <v-tab>シラバス</v-tab>
+      <v-tab>レビュー</v-tab>
+      <v-tab>過去問</v-tab>
+
+      <v-tab-item class="tab">
+        <v-row>
+          <v-col>教授名</v-col>
+          <v-col>丸先生</v-col>
+        </v-row>
+      </v-tab-item>
+
+      <v-tab-item class="tab">
+        <v-row>
+          <v-col>教授名</v-col>
+          <v-col>丸先生</v-col>
+        </v-row>
+      </v-tab-item>
+      <v-tab-item class="tab">
+        <v-row> 過去問</v-row>
+      </v-tab-item>
+    </v-tabs>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Review',
+}
+</script>
+
+<style scoped>
+.tab-container {
+  height: 400px;
+  width: 700px;
+  backdrop-filter: blur(2px);
+  background-color: rgba(255, 255, 255, 0.8);
+}
+
+.tab {
+  padding: 20px;
+}
+</style>
+<style>
+.v-tabs-items {
+  background-color: rgba(255, 255, 255, 0) !important;
+}
+</style>
